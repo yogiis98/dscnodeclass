6781eceb95dfb030af68d634790d92e3bf6be698
Yogesh Yadav <yogeshyadavme@gmail.com>
2020-03-20 15:37:36 +0530

    import export

--- fread.js
+++ fread.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 
 
 fs.readFile("my/hi.txt",{encoding:"utf-8"},(err,data)=>{
-    console.log(data);
-    const jsondata = JSON.parse(data)
-    console.log(jsondata.name)
+    if(err){
+        console.log("error in reading file")
+    }else{
+        console.log(data);
+        const jsondata = JSON.parse(data);
+        console.log(jsondata.name);
+    }
 })
+
+
--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const math = require('./math');
+
 const app = express();
-function isLoggedin() {
-  return true
-}
+
+
+
 app.use(bodyParser.json())
 
 app.use(function(req,res,next) {
-  console.log("someone came on server",Date(),req.ip);
-  req.color="blue";
+  console.log("someone came on server");
 next()
 })
 
-app.post("/events/:eventName", function(req, res) {
-  
-  res.json(req.body.address);
+app.get("/calculator", function(req, res) {
+  const op = req.query.op;
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  let result = 0;
+  switch (op) {
+    case "add":
+      result = math.addition(a,b);
+      break;
+    case "sub":
+      result = math.sub(a, b);
+      break;
+    case "mul":
+      result = math.mul(a, b);
+      break;
+    case "div":
+      result = math.div(a, b);
+      break;
+    default:
+      break;
+  }
+  let resp = {
+    result: result,
+    operation: op
+  };
+  res.json(resp);
 });
 
 
